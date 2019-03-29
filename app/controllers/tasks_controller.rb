7174093979015eaee456c649bff4f537bbb8ccdf
Hilomair <very860112@gmail.com>
2019-03-29 23:45:39 +0800

    part7, done

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,47 @@
+class TasksController < ApplicationController
+
+
+    def index
+        @tasks = Task.all
+    end
+
+    def new
+        @task = Task.new
+    end
+
+    def edit
+        @task = Task.find_by(id: params[:id])
+    end
+
+    #create new task
+    def create
+        @task = Task.new(task_params)
+        if @task.save
+            redirect_to tasks_path, notice: "新增任務成功"
+        else
+            render :new
+        end
+    end
+
+    #update task
+    def update
+        @task = Task.find_by(id: params[:id])
+        if @task.update(task_params)
+            redirect_to tasks_path, notice: "更新資料成功"
+        else
+            render :edit
+        end
+    end
+
+    #delete task
+    def destroy
+        @task = Task.find_by(id: params[:id])
+        @task.destroy if @task
+        redirect_to tasks_path, notice: "任務已刪除！"
+    end
+
+    private
+    def task_params
+        params.require(:task).permit(:title,:content)
+    end
+end
---
+++ db/migrate/20190329154100_change_types.rb
@@ -0,0 +1,5 @@
+class ChangeTypes < ActiveRecord::Migration[5.2]
+  def change
+      change_column :tasks, :title, :string
+  end
+end

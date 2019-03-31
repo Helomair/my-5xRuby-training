ccc78abaf0b6152687b8cbb76ed01320f5d33cf7
Hilomair <very860112@gmail.com>
2019-03-31 17:14:19 +0800

    part8, travis debugging

--- spec/feature/task_spec.rb
+++ spec/feature/task_spec.rb
@@ -10,23 +10,23 @@ feature "Get into #new make new task" do
 		click_button 'save'
 		expect(page).to have_text("Testing title")
     end
-#end
+end
 
-#feature "Get into #edit edit task" do
+feature "Get into #edit edit task" do
 	scenario "User visit #edit & send request" do
-        visit "tasks/new"
-		fill_in 'task_title', :with => "Testing title"
-		fill_in 'task_content', :with => "Testing content"
-		click_button 'save'
+#        visit "tasks/new"
+#		fill_in 'task_title', :with => "Testing title"
+#		fill_in 'task_content', :with => "Testing content"
+#		click_button 'save'
         visit "tasks/2/edit"
 		fill_in 'task_title', :with => "edit title"
 		fill_in 'task_content', :with => "edit content"
 		click_button 'save'
 		expect(page).to have_text("edit title")
 	end
-#end
+end
 
-#feature "DELETE a task" do
+feature "DELETE a task" do
 	scenario "send delete to tasks/1" do
 		page.driver.delete("/tasks/1")
         expect(page).to have_text("redirected.")

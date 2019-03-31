12059216650b486322e2d9295bf2456f000c7603
Hilomair <very860112@gmail.com>
2019-03-31 16:20:34 +0800

    part8, add travis ci

--- spec/feature/task_spec.rb
+++ spec/feature/task_spec.rb
@@ -11,15 +11,17 @@ feature "Get into #new make new task" do
 		expect(page).to have_text("Testing title")
 	end
 end
+
 feature "Get into #edit edit task" do
 	scenario "User visit #edit & send request" do
-		visit "tasks/1/edit"
+		visit "tasks/2/edit"
 		fill_in 'task_title', :with => "edit title"
 		fill_in 'task_content', :with => "edit content"
 		click_button 'save'
 		expect(page).to have_text("edit title")
 	end
 end
+
 feature "DELETE a task" do
 	scenario "send delete to tasks/1" do
 		page.driver.delete("/tasks/1")

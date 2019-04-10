5b9609c60451bc387453fbf5bd108004e3eb769e
Hilomair <very860112@gmail.com>
2019-04-10 22:25:34 +0800

    add user model, but new method can't use yet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_05_132819) do
+ActiveRecord::Schema.define(version: 2019_04_10_131700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 2019_04_05_132819) do
     t.datetime "end_time"
     t.integer "status", default: 1, null: false
     t.integer "priority", default: 1, null: false
+    t.integer "user_id", default: 0
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "passwd"
+    t.string "permission"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- spec/feature/task_spec.rb
+++ spec/feature/task_spec.rb
@@ -4,91 +4,101 @@ require 'database_cleaner'
 
 DatabaseCleaner.strategy = :truncation
 
-feature "Get into #index" do
-#	DatabaseCleaner.clean
-	let!(:task1)  {Task.create(id:"20",title:"title1",content:"content1",end_time:"2019-07-02T08:30",priority:"3")}
-	let!(:task2)  {Task.create(id:"10",title:"title2",content:"content2",end_time:"2019-07-09T08:30",priority:"1")}
-    scenario "should sorted by create time & priority" do
-		visit "tasks"
-		expect(page).to have_text("2019-07-02 08:30:00 +0800 編輯 刪除 title2")
-	end
-end
-
-feature "Behaviors in #index" do
-	# 0 => waiting  1 => processing  2 => done
-	let!(:task1) {Task.create(title:"test1",status:"1")}
-	let!(:task2) {Task.create(title:"test2",status:"2")}
-	let!(:task3) {Task.create(title:"test3",status:"3")}
-
-	scenario "fill_in search_box and search" do
-		visit "tasks"
-		fill_in "search", :with => "test1"
-		click_button 'send_search'
-		(expect(page).to_not have_text("test2")) && (expect(page).to_not have_text("test3")) && (expect(page).to have_text("test1"))
+feature "Start test" do
+	let!(:user) {User.create(name:"admin",passwd:"admin",permission:"admin")}
+	feature "Get into #index" do
+	#	DatabaseCleaner.clean
+		let(:task1)  {Task.new(id:"20",title:"title1",content:"content1",end_time:"2019-07-02T08:30",priority:"3")}
+		let(:task2)  {Task.new(id:"10",title:"title2",content:"content2",end_time:"2019-07-09T08:30",priority:"1")}
+	    scenario "should sorted by create time & priority" do
+	    	user.tasks = [task1, task2]
+	    	user.save
+			visit "tasks"
+			expect(page).to have_text("2019-07-02 08:30:00 +0800 編輯 刪除 title2")
+		end
 	end
 
-	scenario "selected all" do
-		visit "tasks"
-		page.find("#all").click
-		#click_button "waiting"
-		(expect(page).to have_text("test2")) && (expect(page).to have_text("test3")) && (expect(page).to have_text("test1"))
+	feature "Behaviors in #index" do
+		# 0 => waiting  1 => processing  2 => done
+		let(:task1) {Task.new(title:"test1",status:"1")}
+		let(:task2) {Task.new(title:"test2",status:"2")}
+		let(:task3) {Task.new(title:"test3",status:"3")}
+		before :each do
+			user.tasks = [task1, task2, task3]
+		end
+		scenario "fill_in search_box and search" do
+			visit "tasks"
+			fill_in "search", :with => "test1"
+			click_button 'send_search'
+			(expect(page).to_not have_text("test2")) && (expect(page).to_not have_text("test3")) && (expect(page).to have_text("test1"))
+		end
+
+		scenario "selected all" do
+			visit "tasks"
+			page.find("#all").click
+			#click_button "waiting"
+			(expect(page).to have_text("test2")) && (expect(page).to have_text("test3")) && (expect(page).to have_text("test1"))
+		end
+
+		scenario "selected waiting" do
+			visit "tasks"
+			page.find("#waiting").click
+			#click_button "waiting"
+			(expect(page).to_not have_text("test2")) && (expect(page).to_not have_text("test3"))
+		end
+
+		scenario "selected processing" do
+			visit "tasks"
+			page.find("#processing").click
+			(expect(page).to_not have_text("test1")) && (expect(page).to_not have_text("test3"))
+		end
+
+		scenario "selected done" do
+			visit "tasks"
+			page.find("#done").click
+			(expect(page).to_not have_text("test2")) && (expect(page).to_not have_text("test1"))
+		end
+	end
+=begin     因沒有使用者，new暫時無法使用
+	feature "Get into #new make new task" do
+		scenario "User visit #new & send request & title not nil" do
+			visit "tasks/new"
+			fill_in 'task_title', :with => "Testing title"
+			fill_in 'task_content', :with => "Testing content"
+			fill_in 'task_end_time', :with => "2019-07-09T08:30"
+			click_button 'save'
+			expect(page).to have_text("Testing title")
+	    end
+	    scenario "User visit #new & send request & title is nil" do
+			visit "tasks/new"
+			fill_in 'task_title', :with => ""
+			fill_in 'task_content', :with => "Testing content"
+			fill_in 'task_end_time', :with => "2019-07-09T08:30"
+			click_button 'save'
+			expect(page).to(have_text("標題不可為空")) || expect(page).to(have_text("Title connot be null")) 
+	    end
+	end
+=end
+	feature "Get into #edit edit task" do
+		scenario "User visit #edit & send request" do
+	        task = Task.create(title:"Testing title",content:"Testing content")
+	        user.tasks << task
+	    	#user.save
+	        visit "tasks/#{task.id}/edit"
+			fill_in 'task_title', :with => "edit title"
+			fill_in 'task_content', :with => "edit content"
+			fill_in 'task_end_time', :with => "2019-07-09T08:30"
+			click_button 'save'
+			expect(page).to have_text("edit title")
+		end
 	end
 
-	scenario "selected waiting" do
-		visit "tasks"
-		page.find("#waiting").click
-		#click_button "waiting"
-		(expect(page).to_not have_text("test2")) && (expect(page).to_not have_text("test3"))
-	end
-
-	scenario "selected processing" do
-		visit "tasks"
-		page.find("#processing").click
-		(expect(page).to_not have_text("test1")) && (expect(page).to_not have_text("test3"))
-	end
-
-	scenario "selected done" do
-		visit "tasks"
-		page.find("#done").click
-		(expect(page).to_not have_text("test2")) && (expect(page).to_not have_text("test1"))
-	end
-end
-
-feature "Get into #new make new task" do
-	scenario "User visit #new & send request & title not nil" do
-		visit "tasks/new"
-		fill_in 'task_title', :with => "Testing title"
-		fill_in 'task_content', :with => "Testing content"
-		fill_in 'task_end_time', :with => "2019-07-09T08:30"
-		click_button 'save'
-		expect(page).to have_text("Testing title")
-    end
-    scenario "User visit #new & send request & title is nil" do
-		visit "tasks/new"
-		fill_in 'task_title', :with => ""
-		fill_in 'task_content', :with => "Testing content"
-		fill_in 'task_end_time', :with => "2019-07-09T08:30"
-		click_button 'save'
-		expect(page).to(have_text("標題不可為空")) || expect(page).to(have_text("Title connot be null")) 
-    end
-end
-
-feature "Get into #edit edit task" do
-	scenario "User visit #edit & send request" do
-        task = Task.create(title:"Testing title",content:"Testing content")
-        visit "tasks/#{task.id}/edit"
-		fill_in 'task_title', :with => "edit title"
-		fill_in 'task_content', :with => "edit content"
-		fill_in 'task_end_time', :with => "2019-07-09T08:30"
-		click_button 'save'
-		expect(page).to have_text("edit title")
-	end
-end
-
-feature "DELETE a task" do
-	scenario "send delete to tasks/1" do
-         task = Task.create(title:"Testing title",content:"Testing content")
-         page.driver.delete("/tasks/#{task.id}")
-        expect(page).to have_text("redirected.")
+	feature "DELETE a task" do
+		scenario "send delete to tasks/1" do
+	        task = Task.create(title:"Testing title",content:"Testing content")
+	        user.tasks << task
+	        page.driver.delete("/tasks/#{task.id}")
+	        expect(page).to have_text("redirected.")
+		end
 	end
 end

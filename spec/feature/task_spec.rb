b390ef1874e8835179a5f1c08dcf5b9a0a397093
Hilomair <very860112@gmail.com>
2019-04-13 14:34:44 +0800

    part20,done

--- spec/feature/task_spec.rb
+++ spec/feature/task_spec.rb
@@ -195,7 +195,7 @@ feature "Start test" do
 
 		scenario "Can't send delete to tasks/{task.id} without Login" do
 	        page.driver.delete("/tasks/#{task.id}")
-	        expect(page).to have_text("Please log in")
+	        expect(page).to have_text("redirected")
 		end
 		scenario "Send delete to tasks/{task.id}, Logged in" do
 			visit "login"

bbf55b7caae4a9d0b2aa7f62336b4f5b9e32c280
Hilomair <very860112@gmail.com>
2019-04-21 15:17:59 +0800

    part24 done, part23 not yet

--- spec/controllers/errors_controller_spec.rb
+++ spec/controllers/errors_controller_spec.rb
@@ -1,19 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe ErrorsController, type: :controller do
-
-  describe "GET #not_fount" do
-    it "returns http success" do
-      get :not_fount
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #internal_server_error" do
-    it "returns http success" do
-      get :internal_server_error
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
